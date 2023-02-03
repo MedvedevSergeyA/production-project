82bf0e3297b6fedbdf645fbd955d93221e65ad1c
Sergey <medvedev.sergeyaa@gmail.com>
2023-02-03 21:06:09 +0500

    initial commit

---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,7 @@
+import {lazy} from "react";
+
+export const MainPageAsync = lazy(() => new Promise(resolve => {
+    // @ts-ignore
+    // Так в реальных проектах не делать! Делаем только для курса!
+    setTimeout(() => resolve(import('./MainPage')), 1500)
+}))
