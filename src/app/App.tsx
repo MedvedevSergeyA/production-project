42bebd7b02413f01d3910f960bfecb5692a2a877
Sergey <medvedev.sergeyaa@gmail.com>
2023-02-07 20:19:09 +0500

    create routeConfig/AppRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
 import React, {Suspense, useContext} from 'react';
 import './styles/index.scss';
-import {Link, Route, Routes} from "react-router-dom";
+import {Link} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 
 
@@ -16,12 +15,7 @@ const App = () => {
             <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />}/>
-                    <Route path={'/'} element={<MainPage />}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 };
