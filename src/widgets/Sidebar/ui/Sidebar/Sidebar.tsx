0343056e9957fe923fdd1e29d89e376575846bfa
Sergey <medvedev.sergeyaa@gmail.com>
2023-02-18 00:30:22 +0500

    fix linter

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -21,12 +21,12 @@ export const Sidebar = ({ className }: SidebarProps) => {
             data-testid="sidebar"
             className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
-            {/* <Button */}
-            {/*    data-testid="sidebar-toggle" */}
-            {/*    onClick={onToggle} */}
-            {/* > */}
-            {/*    -*/}
-            {/* </Button> */}
+            <Button
+                data-testid="sidebar-toggle"
+                onClick={onToggle}
+            >
+                -
+            </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
