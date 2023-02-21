86890251ee018c80e9c1074589747d46c60d8378
Sergey <medvedev.sergeyaa@gmail.com>
2023-02-22 01:05:57 +0500

    add modal, portal, change header

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
         'react',
         '@typescript-eslint',
         'i18next',
+        'react-hooks',
     ],
     rules: {
         'react/jsx-indent': [2, 4],
@@ -42,16 +43,21 @@ module.exports = {
         'i18next/no-literal-string': [
             'error', { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] },
         ],
-        'max-len': ['error', { ignoreComments: true, code: 105 }],
+        'max-len': ['error', { ignoreComments: true, code: 150 }],
+        'jsx-a11y/no-static-element-interactions': 'off',
+        'jsx-a11y/click-events-have-key-events': 'off',
+        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies
     },
     globals: {
         __IS_DEV__: true,
     },
     overrides: [
         {
-            files: ['**/src/**/*.test.{ts,tsx}'],
+            files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
             rules: {
                 'i18next/no-literal-string': 'off',
+                'max-len': 'off',
             },
         },
     ],
